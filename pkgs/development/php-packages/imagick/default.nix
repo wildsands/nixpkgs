8096ec799765c482a899ebfe951d1606a620cf27
Jonas Heinrich <onny@project-insanity.org>
2025-12-27 16:52:01 +0100

    phpPackages.imagick: fix cross compilation
    (cherry picked from commit 7a2727154913a70f3c7102e608a608a3e7c70672)

--- pkgs/development/php-packages/imagick/default.nix
+++ pkgs/development/php-packages/imagick/default.nix
@@ -1,11 +1,9 @@
 {
   buildPecl,
-  fetchpatch,
   lib,
   imagemagick,
   pkg-config,
   pcre2,
-  php,
 }:
 
 buildPecl {
@@ -15,7 +13,11 @@ buildPecl {
   sha256 = "sha256-vaZ0YchU8g1hBXgrdpxST8NziLddRIHZUWRNIWf/7sY=";
 
   configureFlags = [ "--with-imagick=${imagemagick.dev}" ];
+
+  depsBuildBuild = [ pkg-config ];
+
   nativeBuildInputs = [ pkg-config ];
+
   buildInputs = [ pcre2 ];
 
   meta = {
