d0a1b36ceaa104d784feb1386a914710a28ed7cb
Alexandre Esteves <alexfmpe@proton.me>
2026-01-16 22:11:57 +0000

    haskellPackages.iserv-proxy: workaround runtime failure due to pseudo relocation

--- pkgs/development/haskell-modules/configuration-windows.nix
+++ pkgs/development/haskell-modules/configuration-windows.nix
@@ -14,6 +14,13 @@ with haskellLib;
     else
       super.network;
 
+  # Workaround for
+  #   Mingw-w64 runtime failure:
+  #   32 bit pseudo relocation at 00000001400EB99E out of range, targeting 00006FFFFFEB8170, yielding the value 00006FFEBFDCC7CE.
+  # Root cause seems to be undefined references to libffi as shown by linking errors if we instead use "-Wl,--disable-auto-import"
+  # See https://github.com/rust-lang/rust/issues/132226#issuecomment-2445100058
+  iserv-proxy = appendConfigureFlag "--ghc-option=-optl=-Wl,--disable-runtime-pseudo-reloc" super.iserv-proxy;
+
   # Avoids a cycle by disabling use of the external interpreter for the packages that are dependencies of iserv-proxy.
   # See configuration-nix.nix, where iserv-proxy and network are handled.
   # On Windows, network depends on temporary (see above), which depends on random, which depends on splitmix.
